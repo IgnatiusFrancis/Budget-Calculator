76cd50fa317468b929278a057b15a8b483555fa9
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2022-12-01 17:27:32 +0100

    made changes to main.html

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-let form = document.querySelector("#form"),
-  age = document.querySelector("#age").value,
-  gender = document.querySelector("#gender").value,
-  marital_status = document.querySelector("#marital_status").value,
-  dependent = document.querySelector("#dependent").value,
-  rent = document.querySelector("#rent").value,
-  food = document.querySelector("#food").value,
-  utilities = document.querySelector("#utilities").value,
-  insurance = document.querySelector("#insurance").value,
-  transportation = document.querySelector("#transportation").value,
-  miscelleneous = document.querySelector("#miscelleneous").value;
+let form = document.querySelector("#form");
 
-form.addEventListener("submit", (e) => {
+const formSubmit = (e) => {
   e.preventDefault();
-  const income = document.getElementById("income").value;
+  let income = document.getElementById("income").value,
+    age = document.getElementById("age").value,
+    gender = document.querySelector("#gender").value,
+    marital_status = document.querySelector("#marital_status").value,
+    dependent = document.querySelector("#dependent").value,
+    rent = document.querySelector("#rent").value,
+    food = document.querySelector("#food").value,
+    utilities = document.querySelector("#utilities").value,
+    insurance = document.querySelector("#insurance").value,
+    transportation = document.querySelector("#transportation").value,
+    miscelleneous = document.querySelector("#miscelleneous").value;
 
-  sessionStorage.setItem("income", income);
-  console.log(income);
+  sessionStorage.setItem("income", JSON.stringify(income));
+  sessionStorage.setItem("age", JSON.stringify(age));
+  sessionStorage.setItem("gender", JSON.stringify(gender));
+  sessionStorage.setItem("marital_status", JSON.stringify(marital_status));
+  sessionStorage.setItem("dependent", JSON.stringify(dependent));
+  sessionStorage.setItem("rent", JSON.stringify(rent));
+  sessionStorage.setItem("food", JSON.stringify(food));
+  sessionStorage.setItem("utilities", JSON.stringify(utilities));
+  sessionStorage.setItem("insurance", JSON.stringify(insurance));
+  sessionStorage.setItem("transportation", JSON.stringify(transportation));
+  sessionStorage.setItem("miscelleneous", JSON.stringify(miscelleneous));
+
   window.location.href = "success.html";
-  // const formData = new FormData();
+};
 
-  // alert(
-  //   income
-  //   // age,
-  //   // gender,
-  //   // marital_status,
-  //   // dependent,
-  //   // rent,
-  //   // food,
-  //   // utilities,
-  //   // insurance,
-  //   // transportation,
-  //   // miscelleneous
-  // );
-  //   window.location.href = "success.html";
-});
+form.addEventListener("submit", formSubmit);
